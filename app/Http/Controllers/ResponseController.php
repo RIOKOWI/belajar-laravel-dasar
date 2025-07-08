a393a11143b5db7ae46be0f50f9a9a86f1cfb054
RIOKOWI <achyarrio2004@gmail.com>
2025-07-08 10:32:58 +0700

    RESPONSE : paanggil response fix

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ResponseController extends Controller
 {
-    public function response(Request $request)
+    public function response(Request $request): Response
     {
         return response("Hello response");
     }
